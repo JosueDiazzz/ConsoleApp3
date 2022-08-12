8ec3125b66f604a1708697ce959671bf52ffc85b
Lab Computo <Computosala42022@ugb.edu.sv>
2022-08-12 11:36:21 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese el numero a procesar");
+            int multiplo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("ingrese el limite");
+            int limite = int. (Console.ReadLine());
+
+            for (int i =0; i <= limite; i++)
+            { 
+                if (i %multiplo == 0)
+                {
+                    Console.Write(i + "");
+        }
+    }
+}
